1e3ee1ea40ff17c3775686d1dc59894d5fff0c8b
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-01 14:08:16 -0300

    :hammer salvando audio depois da autenticacao

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { Client, NoAuth } from 'whatsapp-web.js';
+import { Client, NoAuth, Message } from 'whatsapp-web.js';
 import fs from 'fs';
+
+
+
 const qrcode = require('qrcode-terminal') as any;
 
 const app: Express = express();
-const port = process.env.PORT || 3000; // Usa a porta do ambiente se disponível
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,37 +17,62 @@ const client = new Client({
     authStrategy: new NoAuth()
 });
 
-client.on('qr', (qr: string) => {
+
+const userStates: Record<string, boolean> = {};
+
+
+client.on('qr', (qr: string) => {// gera QR code para autenticação
     qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+
+client.on('ready', () => { //depois que autentica pelo qrcode
+    console.log('Client esta pronto!');
 });
 
-client.on('message', async message => {
-    if (message.hasMedia) {
-        try {
-            const media = await message.downloadMedia();
-            if (media.mimetype.startsWith('audio/')) {
-                const filename = `audio-${message.id.id}.mp3`;
-                const filePath = `./audios/${filename}`;
-                fs.writeFileSync(filePath, media.data, 'base64');
-                console.log(`Audio saved: ${filename}`);
+
+client.on('message', async (message: Message) => {
+    const chatId = message.from;
+
+    if (message.body === 'clonevoice1') {    // Ativa gravação
+        userStates[chatId] = true;
+        await message.reply('Gravação de voz ativada. Por favor, envie os áudios.');
+        return;
+    }
+   
+    if (message.body === 'stopclonevoice') { // Desativa gravacao
+        userStates[chatId] = false;
+        await message.reply('Gravação de voz desativada.');
+        return;
+    }
+
+    
+    if (userStates[chatId]) {// processa e salva audios se a gravação estiver ativada
+        if (message.hasMedia) {
+            try {
+                const media = await message.downloadMedia();
+                if (media.mimetype.startsWith('audio/')) {
+                    const filename = `audio-${message.id.id}.mp3`;
+                    const filePath = `./audios/${filename}`;
+                    fs.writeFileSync(filePath, media.data, 'base64');
+                    console.log(`Audio saved: ${filename}`);
+                }
+            } catch (err) {
+                console.error('Error handling media message:', err);
             }
-        } catch (err) {
-            console.error('Error handling media message:', err);
         }
     }
 });
 
+
 client.initialize();
 
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server na porta: ${port}`);
 });
 
-// Garante que o diretório de áudios exista
+
 const audiosDir = './audios';
 if (!fs.existsSync(audiosDir)) {
     fs.mkdirSync(audiosDir);
