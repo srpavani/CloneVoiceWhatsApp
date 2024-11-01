b996e9ee78b2dd5e3ea259d1914ba4c1076280b6
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-01 15:00:56 -0300

    :hammer: refatorando

---
+++ BackEnd/src/app3.ts
@@ -0,0 +1,71 @@
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import { Client, NoAuth, Message } from 'whatsapp-web.js';
+import fs from 'fs';
+import path from 'path';
+
+const qrcode = require('qrcode-terminal') as any;
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const client = new Client({
+    authStrategy: new NoAuth()
+});
+
+client.on('qr', (qr: string) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client está pronto!');
+});
+
+client.on('message', async (message: Message) => {
+    const isFromMe = message.fromMe; 
+    const contactId = isFromMe ? message.to.replace(/[^0-9]/g, '') : message.from.replace(/[^0-9]/g, '');
+    const contactDir = `./contacts/${contactId}`;
+
+
+    if (!fs.existsSync(contactDir)) {
+        fs.mkdirSync(contactDir, { recursive: true });
+        fs.mkdirSync(`${contactDir}/audios`);
+        fs.mkdirSync(`${contactDir}/images`);
+        fs.mkdirSync(`${contactDir}/videos`);
+    }
+
+
+    const textFile = `${contactDir}/conversation.txt`;
+    const sender = isFromMe ? 'Você' : 'Contato';
+    const textContent = `${new Date(message.timestamp * 1000).toLocaleString()} (${sender}): ${message.body}\n`;
+    fs.appendFileSync(textFile, textContent, { encoding: 'utf-8' });
+    console.log(`Texto atualizado: ${textFile}`);
+
+
+    if (message.hasMedia) {
+        const media = await message.downloadMedia();
+        let mediaPath = `${contactDir}/`;
+
+
+        if (media.mimetype.startsWith('audio/')) {
+            mediaPath += 'audios';
+        } else if (media.mimetype.startsWith('image/')) {
+            mediaPath += 'images';
+        } else if (media.mimetype.startsWith('video/')) {
+            mediaPath += 'videos';
+        }
+
+        const filename = `${mediaPath}/${message.timestamp}-${message.id.id}.${media.mimetype.split('/')[1]}`;
+        fs.writeFileSync(filename, media.data, 'base64');
+        console.log(`Mídia salva: ${filename}`);
+    }
+});
+
+client.initialize();
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`);
+});
