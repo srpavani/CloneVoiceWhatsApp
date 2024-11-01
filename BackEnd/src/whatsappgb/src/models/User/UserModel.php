39a33b661088558b5762ece4999f307859f648ec
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-01 14:35:44 -0300

    add funcao whatsappgb

---
+++ BackEnd/src/whatsappgb/src/models/User/UserModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace BackEnd\Src\Whatsappgb\Src\Models\User;
+class User{
+    private $name;
+    private $birthday;
+    private $password;
+
+    private $email;
+
+}
---
+++ BackEnd/src/whatsappgb/src/models/whatsappModel/whatsappModel.php
@@ -0,0 +1,13 @@
+
+<?php
+namespace BackEnd\Src\Whatsappgb\Src\Models\WhatsappModel;
+
+class WhatsappModel{
+
+    private $qrCode;
+    private $contato; 
+    private $conversa;
+    private $imagem;
+    private $audio;
+
+}
